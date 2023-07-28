f61600085881eba2724f6eaec8cb522f476c358e
loic <loic@code-rhapsodie.fr>
2023-07-28 08:54:54 +0200

    add export on users

--- bundle/Controller/Admin/ExportController.php
+++ bundle/Controller/Admin/ExportController.php
@@ -7,6 +7,7 @@ namespace Novactive\Bundle\eZMailingBundle\Controller\Admin;
 use Doctrine\DBAL\Connection;
 use Novactive\Bundle\eZMailingBundle\Entity\MailingList;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,25 +18,44 @@ class ExportController
 {
     /**
      * @Route("/mailing-list/{mailinglist}", name="novaezmailing_mailinglist_export")
+     *
      * @Security("is_granted('view', mailinglist)")
      */
-    public function showAction(MailingList $mailinglist,
-                               Connection  $connection,
+    public function showAction(
+        MailingList $mailinglist,
+        Connection  $connection,
     )
     {
-        $sql = "SELECT u.USER_email, u.USER_first_name, u.USER_last_name, u.USER_gender, u.USER_status
+        $sql = 'SELECT u.USER_email, u.USER_first_name, u.USER_last_name, u.USER_gender, u.USER_status
 from novaezmailing_user u
 inner join novaezmailing_registrations nr on u.USER_id = nr.USER_id
-where nr.ML_id = ?";
+where nr.ML_id = ?';
 
-        return new StreamedResponse(function () use ($connection, $sql, $mailinglist) {
+        return new StreamedResponse($this->generate($connection, $sql, [$mailinglist->getId()]), headers: ['Content-Type' => 'text/csv; charset=utf-8', 'Content-Disposition' => 'attachment; filename="' . urlencode(str_replace(' ', '_', $mailinglist->getName())) . '.csv"']);
+    }
+
+    /**
+     * @Route("/users", name="novaezmailing_users_export")
+     *
+     */
+    public function exportUsersAction(Connection $connection)
+    {
+        $sql = 'SELECT u.USER_email, u.USER_first_name, u.USER_last_name, u.USER_gender, u.USER_status
+from novaezmailing_user u;';
+
+        return new StreamedResponse($this->generate($connection, $sql), headers: ['Content-Type' => 'text/csv; charset=utf-8', 'Content-Disposition' => 'attachment; filename="users.csv"']);
+    }
+
+    private function generate(Connection $connection, string $sql, $parameters = []): \Closure
+    {
+        return function () use ($connection, $sql, $parameters) {
             $csv = fopen('php://output', 'w+');
-            fputcsv($csv, ['email', 'first_name', 'last_name', 'salutation', 'status'], ";");
+            fputcsv($csv, ['email', 'first_name', 'last_name', 'salutation', 'status'], ';');
 
-            foreach ($connection->iterateAssociative($sql, [$mailinglist->getId()]) as $user) {
-                fputcsv($csv, $user, ";");
+            foreach ($connection->iterateAssociative($sql, $parameters) as $user) {
+                fputcsv($csv, $user, ';');
             }
             fclose($csv);
-        }, headers: ['Content-Type' => 'text/csv; charset=utf-8', 'Content-Disposition' => 'attachment; filename="'.urlencode(str_replace(' ', '_', $mailinglist->getName())).'.csv"']);
+        };
     }
 }
