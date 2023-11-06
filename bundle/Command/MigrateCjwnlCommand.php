345ddd394df314d4049c1935b5dd3f9be64466ef
loic audu <loic@code-rhapsodie.fr>
2023-11-06 10:11:21 +0100

    fix gender migration

--- bundle/Command/MigrateCjwnlCommand.php
+++ bundle/Command/MigrateCjwnlCommand.php
@@ -446,6 +446,7 @@ where cju.email is null";
                 $user = new User();
                 $user
                     ->setEmail($userData->email)
+                    ->setGender($userData->gender)
                     ->setBirthDate($userData->birthDate)
                     ->setCompany($userData->company)
                     ->setFirstName($userData->firstName)
