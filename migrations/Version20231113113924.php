70066aa56552cb583fd20d19077d842128d10e3d
loic audu <loic@code-rhapsodie.fr>
2023-11-13 12:08:38 +0100

    refactoring

--- migrations/Version20231113113924.php
+++ migrations/Version20231113113924.php
@@ -12,12 +12,13 @@ final class Version20231113113924 extends AbstractMigration
     private const TABLES = [
         'novaezmailing_user' => 'mailing_user',
         'novaezmailing_mailing_list' => 'mailing_mailing_list',
-        'novaezmailing_mailing' => 'mailing_mailing',
         'novaezmailing_campaign' => 'mailing_campaign',
+        'novaezmailing_mailing' => 'mailing_mailing',
         'novaezmailing_campaign_mailinglists_destination' => 'mailing_campaign_mailinglists_destination',
         'novaezmailing_confirmation_token' => 'mailing_confirmation_token',
         'novaezmailing_broadcast' => 'mailing_broadcast',
         'novaezmailing_stats_hit' => 'mailing_stats_hit',
+        'novaezmailing_registrations' => 'mailing_registrations'
     ];
 
     public function getDescription(): string
@@ -28,7 +29,7 @@ final class Version20231113113924 extends AbstractMigration
     public function up(Schema $schema): void
     {
         foreach (self::TABLES as $oldTable => $newTable) {
-            $result = $this->connection->executeQuery("SHOW TABLE LIKE Name = '$oldTable';");
+            $result = $this->connection->executeQuery("SHOW TABLE STATUS LIKE '$oldTable';");
 
             if (\count($result->fetchAllAssociative()) === 0) {
                 continue;
