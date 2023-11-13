6d28a55e59d2151ba48cbb4bccfec5af43822b03
loic audu <loic@code-rhapsodie.fr>
2023-11-13 11:49:32 +0100

    refactoring

---
+++ migrations/Version20231113113924.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IbexaMailingBundle;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231113113924 extends AbstractMigration
+{
+    private const TABLES = [
+        'novaezmailing_user' => 'mailing_user',
+        'novaezmailing_mailing_list' => 'mailing_mailing_list',
+        'novaezmailing_mailing' => 'mailing_mailing',
+        'novaezmailing_campaign' => 'mailing_campaign',
+        'novaezmailing_campaign_mailinglists_destination' => 'mailing_campaign_mailinglists_destination',
+        'novaezmailing_confirmation_token' => 'mailing_confirmation_token',
+        'novaezmailing_broadcast' => 'mailing_broadcast',
+        'novaezmailing_stats_hit' => 'mailing_stats_hit',
+    ];
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        foreach (self::TABLES as $oldTable => $newTable) {
+            $result = $this->connection->executeQuery("SHOW TABLE LIKE Name = '$oldTable';");
+
+            if (\count($result->fetchAllAssociative()) === 0) {
+                continue;
+            }
+
+            $this->addSql("INSERT INTO $newTable SELECT * FROM $oldTable");
+        }
+
+        foreach (\array_reverse(self::TABLES) as $oldTable => $newTable) {
+            $this->addSql("DROP TABLE $oldTable");
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
