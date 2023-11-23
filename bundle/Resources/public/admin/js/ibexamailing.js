00d6b3519c95c6f4c4adb8ddee38ec2e13b20cf9
Théotime <theotime@code-rhapsodie.fr>
2023-11-23 10:14:07 +0100

    Ajout du bouton, supp effet hover et ajustement toggle

--- bundle/Resources/public/admin/js/ibexamailing.js
+++ bundle/Resources/public/admin/js/ibexamailing.js
@@ -45,8 +45,10 @@ jQuery(function () {
         div.addEventListener('click', () => {
             const scrollableWrapper = div.closest('.toggle-wrapper').querySelector('.m-tree__scrollable-wrapper');
             const treeContainer = div.closest('.m-tree');
+            const titreNavToggleElement = div.querySelector('.titre-nav-toggle');
+            const toggleSvg = div.querySelector('.ibexa-icon');
 
-            if (!scrollableWrapper || !treeContainer) {
+            if (!scrollableWrapper || !treeContainer || !titreNavToggleElement || !toggleSvg) {
                 return; // Arrêter l'exécution si les éléments nécessaires ne sont pas présents
             }
 
@@ -56,12 +58,27 @@ jQuery(function () {
             if (scrollableWrapper.classList.contains('d-none')) {
                 div.style.width = 'fit-content';
                 treeContainer.style.width = '70px';
+
+                // Ajouter la classe d-none à l'élément avec la classe "titre-nav-toggle"
+                titreNavToggleElement.classList.add('d-none');
+
+                // Faire tourner le SVG de 180 degrés avec une transition de 0.3s
+                toggleSvg.style.transition = 'transform 0.3s';
+                toggleSvg.style.transform = 'rotate(180deg)';
+
                 return; // Sortir de la fonction si d-none est active
             }
 
             // Réinitialiser la largeur si d-none n'est pas active
             div.style.width = '';
             treeContainer.style.width = '320px';
+
+            // Supprimer la classe d-none de l'élément avec la classe "titre-nav-toggle"
+            titreNavToggleElement.classList.remove('d-none');
+
+            // Réinitialiser la rotation du SVG avec une transition de 0.3s
+            toggleSvg.style.transition = 'transform 0.3s';
+            toggleSvg.style.transform = '';
         });
     });
 
